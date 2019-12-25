ed635abc855e8744ae8a8d7f79dde28440117826
sali011 <sali011@gold.ac.uk>
2019-12-25 13:45:00 +0000

    Login and Registration Activities Created

---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Login_form.java
@@ -0,0 +1,20 @@
+package com.example.vehicleservicemanagementapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// Main Function
+public class Login_form extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Signup_Form.java
@@ -0,0 +1,19 @@
+package com.example.vehicleservicemanagementapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// Main Function
+public class Signup_Form extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup__form);
+    }
+
+
+}
