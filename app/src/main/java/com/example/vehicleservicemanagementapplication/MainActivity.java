2bea7a8448cad3f69c6993193bf386f7ef6762a1
sali011 <sali011@gold.ac.uk>
2019-12-25 13:10:50 +0000

    Android Studio - Commit Test

--- app/src/main/java/com/example/vehicleservicemanagementapplication/MainActivity.java
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+// Main Function
+public class MainActivity extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
