26b5ad06d968e10166c829ee7eb1e3778d146386
sali011 <sali011@gold.ac.uk>
2019-12-25 19:11:32 +0000

    Main Screen Linked to Login and Registration Screen

--- app/src/main/java/com/example/vehicleservicemanagementapplication/MainActivity.java
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/MainActivity.java
@@ -3,18 +3,35 @@ package com.example.vehicleservicemanagementapplication;
 // Import statements
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-// Main Function
+// Main Class
 public class MainActivity extends AppCompatActivity
 {
     @Override
+    // On create function
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    // Main Screen Login Button Function (take you to login page)
+    public void btn_main_login(View view)
+    {
+        // This opens the 'Login_form' activity once the login button is selected
+        startActivity(new Intent(getApplicationContext(), Login_form.class));
+    }
+
+    // Main Screen Register Button Function (take you to register page)
+    public void btn_main_register(View view)
+    {
+        // This opens the 'Signup_form' activity once the register button is selected
+        startActivity(new Intent(getApplicationContext(), Signup_Form.class));
+    }
 
 
 }
+
