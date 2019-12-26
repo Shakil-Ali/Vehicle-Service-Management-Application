1267def8ded62ed25d87e9b3a93074c46cce6fdf
sali011 <sali011@gold.ac.uk>
2019-12-26 18:34:22 +0000

    Main Menu Activity Added

---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Main_Menu.java
@@ -0,0 +1,19 @@
+package com.example.vehicleservicemanagementapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// Main Class
+public class Main_Menu extends AppCompatActivity {
+
+    @Override
+    // Main Method
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__menu);
+    }
+
+
+}
