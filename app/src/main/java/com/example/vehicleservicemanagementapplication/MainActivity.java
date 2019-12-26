1f72c3e976cfc34d0a6026b35dd32f97f8819d0e
sali011 <sali011@gold.ac.uk>
2019-12-26 22:55:34 +0000

    Create Registration Portal - Make Firebase Authenticate and Store User Details upon Registration

--- app/src/main/java/com/example/vehicleservicemanagementapplication/MainActivity.java
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/MainActivity.java
@@ -6,32 +6,82 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 // Main Class
 public class MainActivity extends AppCompatActivity
 {
+    // Initialise variables
+    private Button btn_home_log;
+    private Button btn_home_reg;
+
+
     @Override
     // On create function
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assign elements to variables
+        btn_home_log = (Button) findViewById(R.id.btn_home_login);
+        btn_home_reg = (Button) findViewById(R.id.btn_home_register);
+
+        // Open Login page
+        btn_home_log.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openLoginPage();
+            }
+        });
+
+        // Open Register Page
+        btn_home_reg.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openRegisterPage();
+            }
+        });
+
+
+    // End of main method
     }
 
-    // Main Screen Login Button Function (take you to login page)
-    public void btn_main_login(View view)
+
+    // Open login page method
+    public void openLoginPage()
     {
-        // This opens the 'Login_form' activity once the login button is selected
-        startActivity(new Intent(getApplicationContext(), Login_form.class));
+        Intent intent1 = new Intent(this, Login_form.class);
+        startActivity(intent1);
     }
 
-    // Main Screen Register Button Function (take you to register page)
-    public void btn_main_register(View view)
+    // Open register page method
+    public void openRegisterPage()
     {
-        // This opens the 'Signup_form' activity once the register button is selected
-        startActivity(new Intent(getApplicationContext(), Signup_Form.class));
+        Intent intent2 = new Intent(this, Signup_Form.class);
+        startActivity(intent2);
     }
 
 
+//    // Main Screen Login Button Function (take you to login page)
+//    public void btn_main_login(View view)
+//    {
+//        // This opens the 'Login_form' activity once the login button is selected
+//        startActivity(new Intent(getApplicationContext(), Login_form.class));
+//    }
+//
+//    // Main Screen Register Button Function (take you to register page)
+//    public void btn_main_register(View view)
+//    {
+//        // This opens the 'Signup_form' activity once the register button is selected
+//        startActivity(new Intent(getApplicationContext(), Signup_Form.class));
+//    }
+
+
+//End of class
 }
 
--- app/src/main/java/com/example/vehicleservicemanagementapplication/Signup_Form.java
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Signup_Form.java
@@ -1,19 +1,124 @@
 package com.example.vehicleservicemanagementapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 // Main Function
 public class Signup_Form extends AppCompatActivity
 {
+    // https://www.youtube.com/watch?v=A_-rTWhIvJQ
+    // Variable Initialisation
+    EditText txtEmail;
+    EditText txtPassword;
+    EditText txtConfirmPassword;
+    Button btn_register;
+
+    // FirebaseAuth variable
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup__form);
+
+        // Set variables to elements
+        txtEmail = (EditText)findViewById(R.id.txt_email);
+        txtPassword = (EditText)findViewById(R.id.txt_password);
+        txtConfirmPassword = (EditText)findViewById(R.id.txt_confirm_password);
+        btn_register = (Button)findViewById(R.id.buttonRegister);
+
+
+        // Sets the firebaseAuth variable to the instance being used
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btn_register.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Intialise new variables to store data of entered fields
+                String email = txtEmail.getText().toString().trim();
+                String password = txtPassword.getText().toString().trim();
+                String confirmPassword = txtConfirmPassword.getText().toString().trim();
+
+                // Validations for fields
+                // Check if an email has been entered
+                if(TextUtils.isEmpty(email))
+                {
+                    // If user has not entered an email, will display toast
+                    Toast.makeText(Signup_Form.this, "Please Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Check if a password has been entered
+                if(TextUtils.isEmpty(password))
+                {
+                    // If user has not entered a password, will display toast
+                    Toast.makeText(Signup_Form.this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Check if a confirmed password has been entered
+                if(TextUtils.isEmpty(confirmPassword))
+                {
+                    // If user has not entered a confirmation of password, will display toast
+                    Toast.makeText(Signup_Form.this, "Please Confirm Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Check if password is below 6 characters
+                if(password.length()<6)
+                {
+                    // Inform user password to short
+                    Toast.makeText(Signup_Form.this, "Password is too short", Toast.LENGTH_SHORT).show();
+                }
+
+                // Code within conditional from Android Studio Firebase Assistant
+                if(password.equals(confirmPassword))
+                {
+                    // Creating a user
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(Signup_Form.this, new OnCompleteListener<AuthResult>()
+                            {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task)
+                                {
+
+                                    // Check if creation successful
+                                    if (task.isSuccessful())
+                                    {
+                                        // If successful, go to main menu
+                                        startActivity(new Intent(getApplicationContext(), Main_Menu.class));
+                                        // Inform user registration is successful
+                                        Toast.makeText(Signup_Form.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                                                            }
+                                    else {
+                                        // Inform user registration has failed
+                                        Toast.makeText(Signup_Form.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
+
+
+
+    // Main Method End
     }
 
 
+// Class ending
 }
