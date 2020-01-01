dd8e31ae4b0d492dd9597af081a03afb40a1089d
sali011 <sali011@gold.ac.uk>
2020-01-01 16:48:47 +0000

    Vehicle Inventory Activity Created

---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/VehicleInventory.java
@@ -0,0 +1,16 @@
+package com.example.vehicleservicemanagementapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VehicleInventory extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_inventory);
+    }
+}
