759dfd5fc9ee839dac4689cabbe5e887181a1480
sali011 <sali011@gold.ac.uk>
2020-01-01 21:40:47 +0000

    Vehicle Image Upload and Reg Number Text Addition - Completed

--- app/src/main/java/com/example/vehicleservicemanagementapplication/VehicleInventory.java
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/VehicleInventory.java
@@ -1,5 +1,6 @@
 package com.example.vehicleservicemanagementapplication;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,20 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
 
@@ -23,6 +38,9 @@ public class VehicleInventory extends AppCompatActivity
     ImageView imageView;
     EditText editText;
     Uri uriUploadedVehicleImage;
+    ProgressBar progressBar;
+    String vehicleImageURL;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,8 +49,10 @@ public class VehicleInventory extends AppCompatActivity
         setContentView(R.layout.activity_vehicle_inventory);
 
         // Assignment Operations
+        firebaseAuth = FirebaseAuth.getInstance();
         imageView = (ImageView)findViewById(R.id.imageView);
         editText = (EditText)findViewById(R.id.editTextDisplayName);
+        progressBar = findViewById(R.id.progressBar);
 
         imageView.setOnClickListener(new View.OnClickListener()
         {
@@ -44,8 +64,61 @@ public class VehicleInventory extends AppCompatActivity
             }
         });
 
+        // Save button on click functionality
+        findViewById(R.id.buttonSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                // Method to save the vehicles information
+                saveVehicleInformation();
+            }
+
+        });
+
     // end of main method method
     }
+
+
+    // Method to save vehicle information
+    private void saveVehicleInformation()
+    {
+        // Store entered text
+        String vehicleReg = editText.getText().toString();
+
+        // Validations
+        if(vehicleReg.isEmpty())
+        {
+            editText.setError("Vehicle Registration Required");
+            editText.requestFocus();
+            return;
+        }
+
+        // Store current logged in user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        // Validations
+        if(user != null && vehicleImageURL != null)
+        {
+            UserProfileChangeRequest profile = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(vehicleReg)
+                    .setPhotoUri(Uri.parse(vehicleImageURL))
+                    .build();
+
+            user.updateProfile(profile)
+                        .addOnCompleteListener(new OnCompleteListener<Void>()
+                        {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task)
+                            {
+                                if(task.isSuccessful())
+                                {
+                                    Toast.makeText(VehicleInventory.this, "Vehicle Data Updated", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+        }
+
+    }
+
 
     // Override onActivityResult method to enable image upload
     @Override
@@ -64,12 +137,55 @@ public class VehicleInventory extends AppCompatActivity
             {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uriUploadedVehicleImage);
                 imageView.setImageBitmap(bitmap);
+                uploadImageToFirebaseStorage();
             }
             catch (IOException e)
             {
                 e.printStackTrace();
             }
         }
+    // end of method
+    }
+
+    // Storing image to Firebase Database Method
+    private void uploadImageToFirebaseStorage()
+    {
+        // Set path for where image should be stored
+        StorageReference vehicleImageReference =
+                FirebaseStorage.getInstance().getReference("vehicleImages/"+System.currentTimeMillis() + ".jpg");
+
+        // Check if image uploaded
+        if(uriUploadedVehicleImage != null)
+        {
+            progressBar.setVisibility(View.VISIBLE);
+            // Store the uploaded image
+            vehicleImageReference.putFile(uriUploadedVehicleImage)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>()
+                    {
+                        // Method for if action is successful
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+                        {
+                            // Hide progress bar
+                            progressBar.setVisibility(View.GONE);
+
+                            // Store image url
+                            vehicleImageURL = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                        }
+                    })
+            .addOnFailureListener(new OnFailureListener()
+            {
+                // Method for handling failures
+                @Override
+                public void onFailure(@NonNull Exception e)
+                {
+                    // Hide progress bar
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(VehicleInventory.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
     // end of method
     }
 
