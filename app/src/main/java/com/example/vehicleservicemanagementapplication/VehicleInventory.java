648720598125b93e6ab15198406e812e9bffaad3
sali011 <sali011@gold.ac.uk>
2020-01-02 21:13:48 +0000

    Vehicle Data Saving to Firebase Database - Complete

--- app/src/main/java/com/example/vehicleservicemanagementapplication/VehicleInventory.java
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/VehicleInventory.java
@@ -27,6 +27,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -43,6 +45,8 @@ public class VehicleInventory extends AppCompatActivity
     EditText editTextReg;
     Button saveButton;
     Spinner spinnerMake;
+    DatabaseReference databaseVehicles;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -54,6 +58,8 @@ public class VehicleInventory extends AppCompatActivity
         editTextReg = (EditText)findViewById(R.id.editRegText);
         saveButton = (Button) findViewById(R.id.buttonSave);
         spinnerMake = (Spinner) findViewById(R.id.spinnerVehicleMake);
+        databaseVehicles = FirebaseDatabase.getInstance().getReference("Vehicles");
+
 
         // Check whether save button is being clicked
         saveButton.setOnClickListener(new View.OnClickListener()
@@ -81,9 +87,17 @@ public class VehicleInventory extends AppCompatActivity
         // Check if registration has been entered
         if(!TextUtils.isEmpty(vehReg))
         {
-
+            // Store database key for this data
+            String id = databaseVehicles.push().getKey();
+            // Create new object
+            Vehicles vehicle = new Vehicles(id, vehReg, vehMake);
+            // Enter object into database (child to retrieve database object) - ensures new node created each time
+            databaseVehicles.child(id).setValue(vehicle);
+            // Message to inform user that vehicle has been added
+            Toast.makeText(this, "Vehicle Added", Toast.LENGTH_SHORT).show();
         }
         else{
+            // Message to inform user that reg has not been entered
             Toast.makeText(this, "Enter Vehicle Registration", Toast.LENGTH_SHORT).show();
         }
 
