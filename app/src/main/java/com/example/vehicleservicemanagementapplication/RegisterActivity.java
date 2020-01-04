8a995718714c14fe2e1329a5f99a70d3902265a8
sali011 <sali011@gold.ac.uk>
2020-01-04 20:29:51 +0000

    New Register Activity Created - Put in Activities Package

---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.vehicleservicemanagementapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    // end of main method
+    }
+
+
+// end of class
+}
