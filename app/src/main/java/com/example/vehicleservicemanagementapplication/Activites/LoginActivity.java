33ffca07c34cb05c7b348e572803e3a7cb979363
sali011 <sali011@gold.ac.uk>
2020-01-05 20:21:25 +0000

    Login Activity created

---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Activites/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vehicleservicemanagementapplication.R;
+
+// https://www.youtube.com/watch?v=wVL-fdcHTj0
+
+public class LoginActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    // end of method
+    }
+
+
+// end of class
+}
