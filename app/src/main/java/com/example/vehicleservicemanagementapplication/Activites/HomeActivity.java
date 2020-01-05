413ab266c4b1b89980acdc4a8b9029b50fd37d34
sali011 <sali011@gold.ac.uk>
2020-01-05 19:18:45 +0000

    Home Activity created

--- app/src/main/java/com/example/vehicleservicemanagementapplication/Activites/HomeActivity.java
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Activites/HomeActivity.java
@@ -6,11 +6,21 @@ import android.os.Bundle;
 
 import com.example.vehicleservicemanagementapplication.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+    // end of main method
     }
+
+
+
+
+// end of class
 }
