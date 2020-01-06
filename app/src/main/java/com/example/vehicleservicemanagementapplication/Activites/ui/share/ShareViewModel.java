ba5179ec2df870cd2f18208119564ea8f25c7005
sali011 <sali011@gold.ac.uk>
2020-01-06 14:05:06 +0000

    Home Nav Added

---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Activites/ui/share/ShareViewModel.java
@@ -0,0 +1,19 @@
+package com.example.vehicleservicemanagementapplication.Activites.ui.share;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ShareViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ShareViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is share fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
