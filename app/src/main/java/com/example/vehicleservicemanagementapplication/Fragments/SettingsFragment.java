371ae71cf9f4c3b7a0f4746fda509fb0645fd9ea
sali011 <sali011@gold.ac.uk>
2020-01-07 13:31:13 +0000

    SettingsFragments class created

--- app/src/main/java/com/example/vehicleservicemanagementapplication/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Fragments/SettingsFragment.java
@@ -17,13 +17,12 @@ import com.example.vehicleservicemanagementapplication.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link HomeFragment.OnFragmentInteractionListener} interface
+ * {@link SettingsFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link SettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-
-public class HomeFragment extends Fragment
+public class SettingsFragment extends Fragment
 {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,7 +35,7 @@ public class HomeFragment extends Fragment
 
     private OnFragmentInteractionListener mListener;
 
-    public HomeFragment()
+    public SettingsFragment()
     {
         // Required empty public constructor
     }
@@ -47,13 +46,14 @@ public class HomeFragment extends Fragment
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment SettingsFragment.
      */
 
+
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2)
+    public static SettingsFragment newInstance(String param1, String param2)
     {
-        HomeFragment fragment = new HomeFragment();
+        SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -77,7 +77,7 @@ public class HomeFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home2, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
 
@@ -121,6 +121,7 @@ public class HomeFragment extends Fragment
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
+
 
     public interface OnFragmentInteractionListener
     {
