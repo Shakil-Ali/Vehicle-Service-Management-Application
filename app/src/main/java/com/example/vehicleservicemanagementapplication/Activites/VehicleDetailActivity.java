eb5e532442ffc52f884f89f281b290eb92467c24
sali011 <sali011@gold.ac.uk>
2020-01-10 17:34:13 +0000

    'Vehicle Detail Activity' class created

---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Activites/VehicleDetailActivity.java
@@ -0,0 +1,29 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+// https://www.youtube.com/watch?v=djvbBM2Kd8A
+
+// Import statements
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vehicleservicemanagementapplication.R;
+
+
+// VehicleDetailActivity class
+public class VehicleDetailActivity extends AppCompatActivity
+{
+    // Main Method
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_detail);
+
+
+    // end of main method
+    }
+
+
+// end of class
+}
