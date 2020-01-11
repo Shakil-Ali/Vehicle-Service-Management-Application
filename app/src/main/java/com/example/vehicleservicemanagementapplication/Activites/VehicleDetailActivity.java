d18cfa95b6ff85389701928b36314653b24b1445
sali011 <sali011@gold.ac.uk>
2020-01-11 13:35:50 +0000

    'Vehicle Detail Activity' - Variable Initialisation & Assignment Operations - Complete

--- app/src/main/java/com/example/vehicleservicemanagementapplication/Activites/VehicleDetailActivity.java
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Activites/VehicleDetailActivity.java
@@ -1,11 +1,14 @@
 package com.example.vehicleservicemanagementapplication.Activites;
 
 // https://www.youtube.com/watch?v=djvbBM2Kd8A
+// https://www.youtube.com/watch?v=UpVw9TRXXaA
 
 // Import statements
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.vehicleservicemanagementapplication.R;
 
@@ -13,12 +16,33 @@ import com.example.vehicleservicemanagementapplication.R;
 // VehicleDetailActivity class
 public class VehicleDetailActivity extends AppCompatActivity
 {
+    // Variable Initialisation
+    // IMAGES
+    ImageView imageVehicle;
+    // TEXT
+    TextView textVehicleTitle;
+    TextView textVehicleDate;
+    TextView textVehicleDescription;
+
+
+
     // Main Method
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicle_detail);
+
+
+        // Assignment Operations
+        // IMAGES
+        imageVehicle = findViewById(R.id.vehicle_detail_image);
+        // TEXT
+        textVehicleTitle = findViewById(R.id.vehicle_detail_title);
+        textVehicleDate = findViewById(R.id.vehicle_detail_date);
+        textVehicleDescription = findViewById(R.id.vehicle_detail_description);
+
+
 
 
     // end of main method
