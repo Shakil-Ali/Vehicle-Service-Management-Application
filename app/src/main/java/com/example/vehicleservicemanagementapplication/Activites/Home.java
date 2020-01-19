e8b016a0fb1f128a6b764ddc77df86d0d3298c6b
sali011 <sali011@gold.ac.uk>
2020-01-19 19:25:54 +0000

    Removed three dot option in Motor Manager (Right Top Corner)

--- app/src/main/java/com/example/vehicleservicemanagementapplication/Activites/Home.java
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Activites/Home.java
@@ -464,24 +464,25 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
 
 
     // Method for on options item selected
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-
-    // end of method
-    }
+    // Method for the three dot in right hand corner
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item)
+//    {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings)
+//        {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//
+//    // end of method
+//    }
 
 
     // Method for on navigation item selected
