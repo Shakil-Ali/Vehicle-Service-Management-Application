5f9b69db4a43a90dc7db0a66e7e368ab97788ff5
sali011 <sali011@gold.ac.uk>
2020-01-19 16:41:52 +0000

    Dark Mode Buttons (Off and On) Initialised and Assigned

--- app/src/main/java/com/example/vehicleservicemanagementapplication/Helpers/NightMode.java
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Helpers/NightMode.java
@@ -3,6 +3,7 @@ package com.example.vehicleservicemanagementapplication.Helpers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.vehicleservicemanagementapplication.R;
 
@@ -10,12 +11,29 @@ import com.example.vehicleservicemanagementapplication.R;
 public class NightMode extends AppCompatActivity
 {
 
+    // Variable Initialisation
+    Button darkModeOnBtn;
+    Button darkModeOffBtn;
+
     // Main Method
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_night_mode);
+
+        // Variable Assignments
+        darkModeOnBtn = findViewById(R.id.darkModeOnButton);
+        darkModeOffBtn = findViewById(R.id.darkModeOffButton);
+
+
+        // On click listener for dark mode on
+
+        // On click listener for dark mode off
+
+
+
+    // end of main method
     }
 
 
