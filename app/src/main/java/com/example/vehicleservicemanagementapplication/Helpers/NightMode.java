15b3526e97e8e7d9fff6feac60e29ef71025c1e6
sali011 <sali011@gold.ac.uk>
2020-01-19 13:22:09 +0000

    Night Mode Class and XML File Created

---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Helpers/NightMode.java
@@ -0,0 +1,23 @@
+package com.example.vehicleservicemanagementapplication.Helpers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vehicleservicemanagementapplication.R;
+
+// Night Mode Class
+public class NightMode extends AppCompatActivity
+{
+
+    // Main Method
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_night_mode);
+    }
+
+
+// end of class
+}
