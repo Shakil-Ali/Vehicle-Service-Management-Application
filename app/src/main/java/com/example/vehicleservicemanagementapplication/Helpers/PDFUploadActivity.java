d616324cdeaf45f9ad8ab4d8b1df9a02da5276ce
sali011 <sali011@gold.ac.uk>
2020-01-27 20:45:51 +0000

    PDFUploadActivity - Created and initial comments

---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Helpers/PDFUploadActivity.java
@@ -0,0 +1,28 @@
+package com.example.vehicleservicemanagementapplication.Helpers;
+
+// https://www.youtube.com/watch?v=lmJHtSChZG0
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vehicleservicemanagementapplication.R;
+
+public class PDFUploadActivity extends AppCompatActivity
+{
+
+    // Main Method
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdfupload);
+
+
+    // end of main method
+    }
+
+
+
+// end of class
+}
