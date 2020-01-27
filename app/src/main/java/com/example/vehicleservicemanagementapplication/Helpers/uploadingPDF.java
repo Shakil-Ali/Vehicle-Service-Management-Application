ff5096bdb62c2ae7c0aa0503090f8b1e2a846a5c
sali011 <sali011@gold.ac.uk>
2020-01-27 21:35:11 +0000

    uploadingPDF java activity created

---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Helpers/uploadingPDF.java
@@ -0,0 +1,12 @@
+package com.example.vehicleservicemanagementapplication.Helpers;
+
+// https://www.youtube.com/watch?v=lmJHtSChZG0
+
+// Uploading PDF Class
+public class uploadingPDF
+{
+
+
+
+// end of class
+}
