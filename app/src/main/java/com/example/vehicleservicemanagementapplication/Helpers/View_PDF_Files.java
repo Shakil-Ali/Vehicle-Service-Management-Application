c7bb647c4a4c48553be142e3ca60e362a386a0a6
sali011 <sali011@gold.ac.uk>
2020-01-28 22:16:30 +0000

    Viewing PDF Files - Activity created

---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/Helpers/View_PDF_Files.java
@@ -0,0 +1,31 @@
+package com.example.vehicleservicemanagementapplication.Helpers;
+
+// https://www.youtube.com/watch?reload=9&v=axChfqYiZwc
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vehicleservicemanagementapplication.R;
+
+
+// Class for viewing PDF files
+public class View_PDF_Files extends AppCompatActivity
+{
+
+    // Main Method
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__pdf__files);
+
+
+
+    // end of main method
+    }
+
+
+
+// end of class
+}
