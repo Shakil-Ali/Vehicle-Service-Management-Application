2f89bc4026c4c2b5183211bef86735caaf6428b4
sali011 <sali011@gold.ac.uk>
2020-03-15 17:23:41 +0000

    LoginActivityUnitTests file created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/LoginActivityUnitTest.java
@@ -0,0 +1,24 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class LoginActivityUnitTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+
+}
