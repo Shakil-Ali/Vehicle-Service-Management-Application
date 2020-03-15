ab49dc2a9b92e5b3fbb1986a08d26f033b98bf0d
sali011 <sali011@gold.ac.uk>
2020-03-15 18:51:41 +0000

    LoginActivityUnitTests - Unit Test - Login function - PASSED

--- app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/LoginActivityUnitTests.java
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/LoginActivityUnitTests.java
@@ -3,14 +3,19 @@ package com.example.vehicleservicemanagementapplication.Activites;
 // https://www.youtube.com/watch?v=_TR6QcRozAg
 // https://www.youtube.com/watch?v=vXRoVIGttO4
 
+import androidx.test.espresso.Espresso;
 import androidx.test.rule.ActivityTestRule;
 
 import com.example.vehicleservicemanagementapplication.MainActivity;
+import com.example.vehicleservicemanagementapplication.R;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 // LoginActivityUnitTests Class (Login screen)
@@ -25,14 +30,36 @@ public class LoginActivityUnitTests
     // Variable to store context
     private LoginActivity nActivity = null;
 
+    // Test credentials
+    private String nEmail = "shakil@gold.ac.uk";
+    private String nPassword = "hello123";
+
 
     // Method for starting up anything before the test
     @Before
     public void setUp() throws Exception
     {
-
+        // Store the context of the activity
+        nActivity = nActivityTestRule.getActivity();
 
     // end of setUp method
+    }
+
+
+    //  Unit test - launching Login Activity and signing in
+    public void testLogin()
+    {
+        // Input email into email field
+        Espresso.onView(withId(R.id.loginEmail)).perform(typeText(nEmail));
+        // Input password into password field
+        Espresso.onView(withId(R.id.loginPassword)).perform(typeText(nPassword));
+        // Close soft keyboard
+        Espresso.closeSoftKeyboard();
+        // Perform button click (on login button)
+        Espresso.onView(withId(R.id.buttonLogin)).perform(click());
+
+
+    // end of testLogin method
     }
 
 
