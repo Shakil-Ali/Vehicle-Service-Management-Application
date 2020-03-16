02123ae86fce5272afbbf7776cbbbfaeb05f5ba7
sali011 <sali011@gold.ac.uk>
2020-03-16 22:07:08 +0000

    HomeUnitTests - File created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/HomeUnitTests.java
@@ -0,0 +1,70 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+// https://www.youtube.com/watch?v=_TR6QcRozAg
+// https://www.youtube.com/watch?v=vXRoVIGttO4
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+// HomeUnitTests class class
+public class HomeUnitTests
+{
+
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+    @Test
+    public void onCreate()
+    {
+
+    }
+
+    @Test
+    public void onActivityResult()
+    {
+
+    }
+
+    @Test
+    public void onBackPressed()
+    {
+
+    }
+
+    @Test
+    public void onCreateOptionsMenu()
+    {
+
+    }
+
+    @Test
+    public void onNavigationItemSelected()
+    {
+
+    }
+
+    @Test
+    public void updateNavHeader()
+    {
+
+    }
+
+
+
+
+// End of HomeUnitTests class
+}
