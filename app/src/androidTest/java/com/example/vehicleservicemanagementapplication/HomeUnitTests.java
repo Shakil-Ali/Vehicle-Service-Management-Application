f112a3e309217ff0fddea1f97db81f579ef1a940
sali011 <sali011@gold.ac.uk>
2020-03-16 22:17:30 +0000

    HomeUnitTests - SetUp and TearDown - code added

--- app/src/androidTest/java/com/example/vehicleservicemanagementapplication/HomeUnitTests.java
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/HomeUnitTests.java
@@ -3,30 +3,58 @@ package com.example.vehicleservicemanagementapplication;
 // https://www.youtube.com/watch?v=_TR6QcRozAg
 // https://www.youtube.com/watch?v=vXRoVIGttO4
 
+import android.app.Instrumentation;
+
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.vehicleservicemanagementapplication.Activites.Home;
+import com.example.vehicleservicemanagementapplication.Activites.LoginActivity;
+import com.example.vehicleservicemanagementapplication.Fragments.HomeFragment;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
 
 
 // HomeUnitTests class class
 public class HomeUnitTests
 {
+    // Variable initialisation and assignment operations
+    // Rule for test - specifies this activity has launched
+    @Rule
+    public ActivityTestRule<Home> nActivityTestRule = new ActivityTestRule<Home>(Home.class);
+    // Variable to store context
+    private Home nActivity = null;
+    // Monitor for home activity
+    Instrumentation.ActivityMonitor monitorHome = getInstrumentation().addMonitor(Home.class.getName(), null, false);
 
 
+    // Method for starting up anything before the test
     @Before
     public void setUp() throws Exception
     {
-
+        // Store the context of the activity
+        nActivity = nActivityTestRule.getActivity();
 
     // end of set up method
     }
 
+
+
+    @Test
+    //  Unit test -
+
+
+
     @After
     public void tearDown() throws Exception
     {
-
+        // Nullify activity after test
+        nActivity = null;
 
     // end of tear down method
     }
