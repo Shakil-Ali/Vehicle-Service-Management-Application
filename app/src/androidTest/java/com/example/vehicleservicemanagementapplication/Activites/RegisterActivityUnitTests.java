28af3d6d98cd128cb50c3d1cc21b8dcdf7b7a8b2
sali011 <sali011@gold.ac.uk>
2020-03-16 20:11:56 +0000

    RegisterActivityUnitTests - Unit Test - Register function - PASSED

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/RegisterActivityUnitTests.java
@@ -0,0 +1,26 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+// RegisterActivityUnitTests class
+public class RegisterActivityUnitTests
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+
+
+}
