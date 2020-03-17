3f0b8d5a8949fcbc160882af7367c2f11f210d9b
sali011 <sali011@gold.ac.uk>
2020-03-17 19:08:21 +0000

    HomeUnitTests - setuppopupimageclick(), openGallery(), checkAndRequestPermission - PASSED

--- app/src/androidTest/java/com/example/vehicleservicemanagementapplication/HomeUnitTests.java
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/HomeUnitTests.java
@@ -3,11 +3,18 @@ package com.example.vehicleservicemanagementapplication;
 // https://www.youtube.com/watch?v=_TR6QcRozAg
 // https://www.youtube.com/watch?v=vXRoVIGttO4
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.Instrumentation;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -31,7 +38,10 @@ import static org.junit.Assert.*;
 
 
 // HomeUnitTests class class
-public class HomeUnitTests {
+public class HomeUnitTests
+{
+
+
     // Variable initialisation and assignment operations
     // Rule for test - specifies this activity has launched
     @Rule
@@ -40,6 +50,11 @@ public class HomeUnitTests {
     private Home nActivity = null;
     // Monitor for home activity
     Instrumentation.ActivityMonitor monitorHome = getInstrumentation().addMonitor(Home.class.getName(), null, false);
+    // Image view variable for pop up post image
+    ImageView popUpPostImage;
+    // Final variable for image addition on pop-up
+    private static final int PReqCode = 2;
+    private static final int REQUESTCODE = 2;
 
 
     // Method for starting up anything before the test
@@ -59,6 +74,70 @@ public class HomeUnitTests {
         Toast.makeText(nActivity, message, Toast.LENGTH_LONG).show();
 
     // end of showMessage method
+    }
+
+
+    @Test
+    // Unit test - set up pop up image click method
+    private void setupPopUpImageClick()
+    {
+        // Detect click on post image area on pop-up
+        popUpPostImage.setOnClickListener(new View.OnClickListener()
+        {
+            // On Click Method
+            @Override
+            public void onClick(View v)
+            {
+                // Check if user has given permission
+                checkAndRequestForPermission();
+
+        // end of on click method
+            }
+        });
+
+    // end of method for setup pop up image click
+    }
+
+    private void checkAndRequestForPermission()
+    {
+        // Check if permission not granted
+        if(ContextCompat.checkSelfPermission(nActivity, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            // Nested conditional statement
+            if(ActivityCompat.shouldShowRequestPermissionRationale(nActivity, Manifest.permission.READ_EXTERNAL_STORAGE))
+            {
+                // Inform the user
+                Toast.makeText(nActivity, "Please accept the required permission", Toast.LENGTH_SHORT).show();
+            }
+            // else conditional
+            else
+            {
+                ActivityCompat.requestPermissions(nActivity,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PReqCode);
+            }
+        }
+        // else conditional
+        else
+        {
+            //  call to method - opens gaellery if user has permission
+            openGallery();
+        }
+
+    // end of checkAndRequestForPermission method
+    }
+
+
+    private void openGallery()
+    {
+        // Initialise and assign new intent variable to hold content
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        // Set the intents type i.e. image
+        galleryIntent.setType("image/*");
+        // Start Activity to achieve this
+
+    // end of openGallery method
     }
 
 
