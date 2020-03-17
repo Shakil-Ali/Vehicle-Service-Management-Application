caadaddb3d68b03e7050ff69e841229385083547
sali011 <sali011@gold.ac.uk>
2020-03-17 22:31:35 +0000

    VehicleDetailActivityUnitTests - File created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/VehicleDetailActivityUnitTests.java
@@ -0,0 +1,34 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+// Class for vehicle detail unit tests
+public class VehicleDetailActivityUnitTests
+{
+
+    // Any actions to carry out before testing
+    @Before
+    public void setUp() throws Exception
+    {
+
+
+    // end of setup method
+    }
+
+
+    // Any actions to carry out after testing
+    @After
+    public void tearDown() throws Exception
+    {
+
+
+    // end of tear down method
+    }
+
+
+// end of vehicle detail activity unit tests class
+}
