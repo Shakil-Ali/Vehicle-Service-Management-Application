b64547334d685810cfb1c017ce2f605f9bf41022
sali011 <sali011@gold.ac.uk>
2020-03-18 14:16:24 +0000

    PostAdapterUnitTests - Methods for unit testing - PASSED

--- app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Adapters/PostAdapterUnitTests.java
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Adapters/PostAdapterUnitTests.java
@@ -4,15 +4,24 @@ package com.example.vehicleservicemanagementapplication.Adapters;
 // https://www.youtube.com/watch?v=vXRoVIGttO4
 
 import android.app.Instrumentation;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.test.rule.ActivityTestRule;
 
+import com.bumptech.glide.Glide;
 import com.example.vehicleservicemanagementapplication.Activites.Home;
+import com.example.vehicleservicemanagementapplication.Models.Post;
+import com.example.vehicleservicemanagementapplication.R;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.List;
 
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
@@ -28,6 +37,11 @@ public class PostAdapterUnitTests
     private PostAdapter nActivity = null;
     // Monitor for home activity
     Instrumentation.ActivityMonitor monitorPostAdapter = getInstrumentation().addMonitor(PostAdapter.class.getName(), null, false);
+    // Variable initialisation and assignment operations
+    // Variable initialisation
+    Context m_Context;
+    List<Post> m_Data;
+    private ViewGroup parent;
 
 
     // Setup method for before testing
@@ -45,7 +59,10 @@ public class PostAdapterUnitTests
     @Test
     public void onCreateViewHolder()
     {
-
+        // Instantiate layout XML file into corresponding view objects
+        View row = LayoutInflater.from(m_Context).inflate(R.layout.row_post_item, parent, false);
+        // Return this view
+//        return new PostAdapterUnitTests().MyViewHolder(row);
 
 
     // end of on create view holder method test
@@ -56,7 +73,10 @@ public class PostAdapterUnitTests
     @Test
     public void onBindViewHolder()
     {
-
+        // Make holder have the title from data in specified position
+//        holder.tv_Title.setText(m_Data.get(position).getTitle());
+//        // Make holder have the image from data in specified position
+//        Glide.with(m_Context).load(m_Data.get(position).getPicture()).into(holder.imageVehicle);
 
     // end of bind view holder method
     }
@@ -64,11 +84,11 @@ public class PostAdapterUnitTests
 
     //
     @Test
-    public void getItemCount()
+    public int getItemCount()
     {
+        return m_Data.size();
 
-
-    // end of get item count method
+        // end of get item count method
     }
 
 
@@ -76,7 +96,8 @@ public class PostAdapterUnitTests
     @After
     public void tearDown() throws Exception
     {
-
+        // Nullify activity after test
+        nActivity = null;
 
     // end of tear down method
     }
