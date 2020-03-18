84647f9d66494af78ba43eb381ceddda3b90593a
sali011 <sali011@gold.ac.uk>
2020-03-18 23:13:19 +0000

    PostUnitTests class - File created and initial comments added

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Models/PostUnitTests.java
@@ -0,0 +1,200 @@
+package com.example.vehicleservicemanagementapplication.Models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+// Post unit tests class
+public class PostUnitTests
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+
+    @Test
+    public void getTitle()
+    {
+
+    }
+
+
+    @Test
+    public void getMake()
+    {
+
+    }
+
+
+    @Test
+    public void getModel()
+    {
+
+    }
+
+
+    @Test
+    public void getTransmission()
+    {
+
+    }
+
+
+    @Test
+    public void getOil()
+    {
+
+    }
+
+
+    @Test
+    public void getDescription()
+    {
+
+    }
+
+
+    @Test
+    public void getNote()
+    {
+
+    }
+
+
+    @Test
+    public void getPicture()
+    {
+
+    }
+
+
+    @Test
+    public void getUserId()
+    {
+
+    }
+
+
+    @Test
+    public void getUserPhoto()
+    {
+
+    }
+
+
+    @Test
+    public void getTimeStamp()
+    {
+
+    }
+
+
+    @Test
+    public void getVehicleKey()
+    {
+
+    }
+
+
+    @Test
+    public void setTitle()
+    {
+
+    }
+
+
+    @Test
+    public void setMake()
+    {
+
+    }
+
+
+    @Test
+    public void setModel()
+    {
+
+    }
+
+
+    @Test
+    public void setTransmission()
+    {
+
+    }
+
+
+    @Test
+    public void setOil()
+    {
+
+    }
+
+
+    @Test
+    public void setDescription()
+    {
+
+    }
+
+
+    @Test
+    public void setNote()
+    {
+
+    }
+
+
+    @Test
+    public void setPicture()
+    {
+
+    }
+
+
+    @Test
+    public void setUserId()
+    {
+
+    }
+
+
+    @Test
+    public void setUserPhoto()
+    {
+
+    }
+
+
+    @Test
+    public void setTimeStamp()
+    {
+
+    }
+
+
+    @Test
+    public void setVehicleKey()
+    {
+
+    }
+
+
+
+
+// end of post unit tests class
+}
+
