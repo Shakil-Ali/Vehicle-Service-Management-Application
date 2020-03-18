b87e7b68ebfff07da0b430db5e2346f698487f70
sali011 <sali011@gold.ac.uk>
2020-03-18 22:30:44 +0000

    HomeFragmentUnitTests - File created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Fragments/ProfileFragmentUnitTests.java
@@ -0,0 +1,39 @@
+package com.example.vehicleservicemanagementapplication.Fragments;
+
+// https://www.youtube.com/watch?v=JoFN10FDm8U
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+// Profile fragment unit tests class
+public class ProfileFragmentUnitTests
+{
+
+    // Initialisations for testing
+
+
+    // Set up method (before tests)
+    @Before
+    public void setUp() throws Exception
+    {
+
+
+    // end of set up method
+    }
+
+
+    // Tear down method (after tests)
+    @After
+    public void tearDown() throws Exception
+    {
+
+
+    // end of tear down method
+    }
+
+
+
+// end of profile fragment unit tests class
+}
