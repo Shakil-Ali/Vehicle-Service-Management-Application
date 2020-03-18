24cc6afa2aacd2dd251894fbf7db3d11063be900
sali011 <sali011@gold.ac.uk>
2020-03-18 15:56:26 +0000

    HomeFragmentUnitTests - File created + Comments Added

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Fragments/HomeFragmentUnitTests.java
@@ -0,0 +1,36 @@
+package com.example.vehicleservicemanagementapplication.Fragments;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+// https://www.youtube.com/watch?v=JoFN10FDm8U
+
+// Home fragment unit tests class
+public class HomeFragmentUnitTests
+{
+
+    // Set up method (before tests)
+    @Before
+    public void setUp() throws Exception
+    {
+
+
+    // end of set up method
+    }
+
+
+    // Tear down method (after tests)
+    @After
+    public void tearDown() throws Exception
+    {
+
+
+    // end of tear down method
+    }
+
+
+
+// end of home fragment unit tests class
+}
