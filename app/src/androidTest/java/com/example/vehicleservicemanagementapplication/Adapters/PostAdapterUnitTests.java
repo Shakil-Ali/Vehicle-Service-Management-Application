e78ae4044ccb0a1182cbf9aefe56160f26cdbf91
sali011 <sali011@gold.ac.uk>
2020-03-18 13:14:42 +0000

    PostAdapterUnitTests - File created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Adapters/PostAdapterUnitTests.java
@@ -0,0 +1,46 @@
+package com.example.vehicleservicemanagementapplication.Adapters;
+
+// https://www.youtube.com/watch?v=_TR6QcRozAg
+// https://www.youtube.com/watch?v=vXRoVIGttO4
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PostAdapterUnitTests
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+    @Test
+    public void onCreateViewHolder()
+    {
+
+    }
+
+    @Test
+    public void onBindViewHolder()
+    {
+
+    }
+
+    @Test
+    public void getItemCount()
+    {
+
+    }
+
+
+}
