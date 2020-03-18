cd48eea7523eec06c09677c2d87bbbf21a778afe
sali011 <sali011@gold.ac.uk>
2020-03-18 22:52:58 +0000

    SettingsFragmentUnitTests - File created and moved

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Fragments/SettingsFragmentUnitTests.java
@@ -0,0 +1,24 @@
+package com.example.vehicleservicemanagementapplication.Fragments;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class SettingsFragmentUnitTests
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+
+}
