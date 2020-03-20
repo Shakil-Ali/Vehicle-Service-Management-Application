f41af1a75fd2350487b67d5e36e9b3064c10ba95
sali011 <sali011@gold.ac.uk>
2020-03-20 22:46:00 +0000

    RegisterToHomeIntegrationTest - Initialisations - Complete

--- app/src/androidTest/java/com/example/vehicleservicemanagementapplication/RegisterToHomeIntegrationTest.java
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/RegisterToHomeIntegrationTest.java
@@ -2,15 +2,34 @@ package com.example.vehicleservicemanagementapplication;
 
 // https://www.youtube.com/watch?v=Xz5Ti4ZoiWA
 
+import android.app.Instrumentation;
+
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.vehicleservicemanagementapplication.Activites.HomeActivity;
+import com.example.vehicleservicemanagementapplication.Activites.RegisterActivity;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
 
 
 // Register to home integration test class
 public class RegisterToHomeIntegrationTest
 {
+
+    // Initialisations
+    // Rule for test
+    @Rule
+    public ActivityTestRule<RegisterActivity> nActivityTestRule = new ActivityTestRule<RegisterActivity>(RegisterActivity.class);
+    // Private variable to initialise profile activity
+    private RegisterActivity nActivity = null;
+    // Monitor the profile activity
+    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(HomeActivity.class.getName(), null, false);
+
 
     // Setup method (before testing)
     @Before
