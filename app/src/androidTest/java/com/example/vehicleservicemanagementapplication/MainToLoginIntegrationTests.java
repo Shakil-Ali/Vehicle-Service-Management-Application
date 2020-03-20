9cadafc9b3017f52343584aee7ff362d055c2031
sali011 <sali011@gold.ac.uk>
2020-03-20 21:20:48 +0000

    Integration Testing - MainToLoginIntegrationTests - File created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/MainToLoginIntegrationTests.java
@@ -0,0 +1,38 @@
+package com.example.vehicleservicemanagementapplication;
+
+// https://www.youtube.com/watch?v=Xz5Ti4ZoiWA
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+// Main To Login Integration Test
+public class MainToLoginIntegrationTests
+{
+
+    // Set up method (occurs before testing)
+    @Before
+    public void setUp() throws Exception
+    {
+
+
+    // end of setup method
+    }
+
+
+    // Tear down method (occurs after testing)
+    @After
+    public void tearDown() throws Exception
+    {
+
+
+    // end of tear down method
+    }
+
+
+
+
+// end of main to login integration unit tests class
+}
