b869f4cbae1e4ab97a835963155e70efb4a8e767
sali011 <sali011@gold.ac.uk>
2020-03-24 17:39:00 +0000

    RegisterActivitySystematicTest - File created and initial methods added

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/RegisterActivitySystematicTest.java
@@ -0,0 +1,38 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+// https://www.youtube.com/watch?v=_TR6QcRozAg
+// https://www.youtube.com/watch?v=vXRoVIGttO4
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+// Register activity systematic test class
+public class RegisterActivitySystematicTest
+{
+
+    // Setup method
+    @Before
+    public void setUp() throws Exception
+    {
+
+
+    // end of set up method
+    }
+
+
+    // Tear down method
+    @After
+    public void tearDown() throws Exception
+    {
+
+
+    // end of tear down method
+    }
+
+
+
+
+// end of register activity systematic test class
+}
