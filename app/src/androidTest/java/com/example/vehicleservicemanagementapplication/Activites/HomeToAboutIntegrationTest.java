04fc5eb172480ad9066eb54c0a6e63062b1d82e8
sali011 <sali011@gold.ac.uk>
2020-03-24 13:44:15 +0000

    HomeToAboutIntegrationTest - Testing method added

--- app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/HomeToAboutIntegrationTest.java
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/HomeToAboutIntegrationTest.java
@@ -2,9 +2,23 @@ package com.example.vehicleservicemanagementapplication.Activites;
 
 // https://www.youtube.com/watch?v=Xz5Ti4ZoiWA
 
+import android.app.Activity;
+import android.app.Instrumentation;
+
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.vehicleservicemanagementapplication.About;
+import com.example.vehicleservicemanagementapplication.R;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
 
 
@@ -12,13 +26,44 @@ import static org.junit.Assert.*;
 public class HomeToAboutIntegrationTest
 {
 
+    // Initialisations
+    // Rule for test
+    @Rule
+    public ActivityTestRule<Home> nActivityTestRule = new ActivityTestRule<Home>(Home.class);
+    // Private variable to initialise home activity
+    private Home nActivity = null;
+    // Monitor the about activity
+    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(About.class.getName(), null, false);
+
+
     // Setup method
     @Before
     public void setUp() throws Exception
     {
-
+        // Get the activity
+        nActivity = nActivityTestRule.getActivity();
 
     // end of setup method
+    }
+
+
+    // Integration Test - Home to About activity
+    @Test
+    public void homeToAbout()
+    {
+        // Check if it does not return null
+        assertNotNull(nActivity.findViewById(R.id.drawer_layout));
+        // Take the view with login button id
+        onView(withId(R.id.drawer_layout)).perform(click());
+        // Wait for monitor to be hit and then expires in 50000000 ms
+        Activity aboutActivity = getInstrumentation().waitForMonitorWithTimeout(monitor, 50000000);
+        // Check that the about activity is not null
+        assertNotNull(aboutActivity);
+        // Finish the opened activity
+        aboutActivity.finish();
+
+
+    // end of home to about method
     }
 
 
@@ -26,7 +71,8 @@ public class HomeToAboutIntegrationTest
     @After
     public void tearDown() throws Exception
     {
-
+        // Set to null and finish
+        nActivity = null;
 
     // end of tear down method
     }
