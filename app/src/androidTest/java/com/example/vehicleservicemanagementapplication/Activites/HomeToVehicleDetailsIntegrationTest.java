71c9d2844a36c46c21c7cb56ffe41d3ee935c2bc
sali011 <sali011@gold.ac.uk>
2020-03-24 14:42:41 +0000

    HomeToVehicleDetailsIntegrationTest - File created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/HomeToVehicleDetailsIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+public class HomeToVehicleDetailsIntegrationTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+
+
+}
