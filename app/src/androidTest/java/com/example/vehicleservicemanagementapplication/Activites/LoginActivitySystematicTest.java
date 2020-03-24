cb8d4c6286942b22ba1dd68ca35341fc29805c1d
sali011 <sali011@gold.ac.uk>
2020-03-24 18:15:29 +0000

    LoginActivitySystematicTest - Setup and tear down methods added

--- app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/LoginActivitySystematicTest.java
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/LoginActivitySystematicTest.java
@@ -3,20 +3,38 @@ package com.example.vehicleservicemanagementapplication.Activites;
 // https://www.youtube.com/watch?v=_TR6QcRozAg
 // https://www.youtube.com/watch?v=vXRoVIGttO4
 
+import android.app.Instrumentation;
+
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.vehicleservicemanagementapplication.Fragments.HomeFragment;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
 
 // Login activity systematic test
 public class LoginActivitySystematicTest
 {
+    // Variable initialisation and assignment operations
+    // Rule for test - specifies this activity has launched
+    @Rule
+    public ActivityTestRule<LoginActivity> nActivityTestRule = new ActivityTestRule<LoginActivity>(LoginActivity.class);
+    // Variable to store context
+    private LoginActivity nActivity = null;
+    // Monitor for home activity
+    Instrumentation.ActivityMonitor monitorHome = getInstrumentation().addMonitor(HomeFragment.class.getName(), null, false);
+
 
     // Set up method
     @Before
     public void setUp() throws Exception
     {
-
+        // Store the context of the activity
+        nActivity = nActivityTestRule.getActivity();
 
     // end of set up method
     }
@@ -26,7 +44,8 @@ public class LoginActivitySystematicTest
     @After
     public void tearDown() throws Exception
     {
-
+        // Nullify activity after test
+        nActivity = null;
 
     // end of tear down method
     }
