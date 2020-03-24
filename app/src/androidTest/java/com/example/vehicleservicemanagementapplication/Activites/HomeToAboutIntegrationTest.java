3f38e7867728e5fdeaff0312458308ec2818c540
sali011 <sali011@gold.ac.uk>
2020-03-24 13:32:34 +0000

    HomeToAboutIntegrationTest - File created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/HomeToAboutIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+public class HomeToAboutIntegrationTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+
+
+
+// end of home to about integration test class
+}
