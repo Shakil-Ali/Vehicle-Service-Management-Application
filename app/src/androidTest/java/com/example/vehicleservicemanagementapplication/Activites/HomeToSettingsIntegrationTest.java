e8a4eb980c49b55cea43fdb25f82d47bd8f9cf69
sali011 <sali011@gold.ac.uk>
2020-03-24 13:52:40 +0000

    HomeToSettingsIntegrationTest - File created and setup/teardown added

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/HomeToSettingsIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+// https://www.youtube.com/watch?v=Xz5Ti4ZoiWA
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+// HomeToSettingsIntegrationTest class
+public class HomeToSettingsIntegrationTest
+{
+
+    // Setup method
+    @Before
+    public void setUp() throws Exception
+    {
+
+
+    // end of setup method
+    }
+
+
+    // Teardown method
+    @After
+    public void tearDown() throws Exception
+    {
+
+
+    // end of tear down method
+    }
+
+
+
+// end of home to settings integration test class
+}
