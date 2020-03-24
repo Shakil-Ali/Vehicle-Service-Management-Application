bb80801de7a04b87e8a205198eff64e808cd4781
sali011 <sali011@gold.ac.uk>
2020-03-24 18:12:04 +0000

    LoginActivitySystematicTest - File created and initial methods added

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/LoginActivitySystematicTest.java
@@ -0,0 +1,37 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+// https://www.youtube.com/watch?v=_TR6QcRozAg
+// https://www.youtube.com/watch?v=vXRoVIGttO4
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+// Login activity systematic test
+public class LoginActivitySystematicTest
+{
+
+    // Set up method
+    @Before
+    public void setUp() throws Exception
+    {
+
+
+    // end of set up method
+    }
+
+
+    // Tear down method
+    @After
+    public void tearDown() throws Exception
+    {
+
+
+    // end of tear down method
+    }
+
+
+
+// end of login activity systematic test
+}
