64622d372f389c43a6aa464f325a301c9ed37313
sali011 <sali011@gold.ac.uk>
2020-03-24 14:45:31 +0000

    HomeToVehicleDetailsIntegrationTest - Setup and tear down method added

--- app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/HomeToVehicleDetailsIntegrationTest.java
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/HomeToVehicleDetailsIntegrationTest.java
@@ -1,27 +1,38 @@
 package com.example.vehicleservicemanagementapplication.Activites;
 
+// https://www.youtube.com/watch?v=Xz5Ti4ZoiWA
+
 import org.junit.After;
 import org.junit.Before;
 
 import static org.junit.Assert.*;
 
 
+// HomeToVehicleDetailsIntegrationTest class
 public class HomeToVehicleDetailsIntegrationTest
 {
 
+    // Setup method
     @Before
     public void setUp() throws Exception
     {
 
+
+    // end of setup method
     }
 
 
+    // Teardown method
     @After
     public void tearDown() throws Exception
     {
 
+
+    // end of tear down method
     }
 
 
 
+
+// end of home to vehicle details integration test class
 }
