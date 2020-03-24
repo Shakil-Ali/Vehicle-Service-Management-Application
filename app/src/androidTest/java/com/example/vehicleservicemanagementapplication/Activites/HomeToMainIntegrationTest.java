e14e47a5f538aefd98f5fb80ac1ec4b7c9484c17
sali011 <sali011@gold.ac.uk>
2020-03-24 14:18:35 +0000

    HomeToMainIntegrationTest (Logout to Opening screen) - Setup/teardown methods added

--- app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/HomeToMainIntegrationTest.java
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/HomeToMainIntegrationTest.java
@@ -1,17 +1,37 @@
 package com.example.vehicleservicemanagementapplication.Activites;
 
+// https://www.youtube.com/watch?v=Xz5Ti4ZoiWA
+
 import org.junit.After;
 import org.junit.Before;
 
 import static org.junit.Assert.*;
 
-public class HomeToMainIntegrationTest {
+// Home to main integration test class
+public class HomeToMainIntegrationTest
+{
 
+    // Setup method
     @Before
-    public void setUp() throws Exception {
+    public void setUp() throws Exception
+    {
+
+
+    // end of set up method
     }
 
+
+    // Tear down method
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() throws Exception
+    {
+
+
+    // end of tear down method
     }
+
+
+
+
+// end of home to main integration test class
 }
