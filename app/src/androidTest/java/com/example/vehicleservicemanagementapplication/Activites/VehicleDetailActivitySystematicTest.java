d803a21efd431926c279859ce2999a77c0b1e980
sali011 <sali011@gold.ac.uk>
2020-03-24 22:29:34 +0000

    VehicleDetailActivitySystematicTest - File created and initial comments

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/VehicleDetailActivitySystematicTest.java
@@ -0,0 +1,39 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+// https://www.youtube.com/watch?v=_TR6QcRozAg
+// https://www.youtube.com/watch?v=vXRoVIGttO4
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+// VehicleDetailActivitySystematicTest
+public class VehicleDetailActivitySystematicTest
+{
+
+    // Setup method
+    @Before
+    public void setUp() throws Exception
+    {
+
+
+    // end of set up method
+    }
+
+
+    // Tear down method
+    @After
+    public void tearDown() throws Exception
+    {
+
+
+    // end of tear down method
+    }
+
+
+
+
+// end of vehicle detail activity systematic test class
+}
