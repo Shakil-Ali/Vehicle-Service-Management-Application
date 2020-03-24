3f952d9c989125251a610f2db542d2b5da60d7ed
sali011 <sali011@gold.ac.uk>
2020-03-24 23:19:07 +0000

    VehicleDetailActivitySystematicTest - Testing method added - PASSED

--- app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/VehicleDetailActivitySystematicTest.java
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/VehicleDetailActivitySystematicTest.java
@@ -5,14 +5,20 @@ package com.example.vehicleservicemanagementapplication.Activites;
 
 import android.app.Instrumentation;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.rule.ActivityTestRule;
 
 import com.example.vehicleservicemanagementapplication.Fragments.HomeFragment;
+import com.example.vehicleservicemanagementapplication.R;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
 
@@ -31,6 +37,16 @@ public class VehicleDetailActivitySystematicTest
     Instrumentation.ActivityMonitor monitorVehicleDetails = getInstrumentation().addMonitor(VehicleDetailActivity.class.getName(), null, false);
 
 
+    // Test registration #1
+    private String reg1 = "SHAKIL";
+
+    // Test registration #2
+    private int reg2 = 123456;
+
+    // Test registration #3
+    private char reg3 = '!';
+
+
     // Setup method
     @Before
     public void setUp() throws Exception
@@ -40,6 +56,84 @@ public class VehicleDetailActivitySystematicTest
 
     // end of set up method
     }
+
+
+    @Test
+    //  Systematic test #1 - launching Login Activity and signing in
+    public void testVehicleDetails1()
+    {
+//        // If view not null, then find element (login button)
+//        assertNotNull(nActivity.findViewById(R.id.loginEmail));
+
+
+        Espresso.onView(withId(R.id.popup_title)).perform(typeText(reg1));
+        // Close soft keyboard
+        Espresso.closeSoftKeyboard();
+        // Perform button click (on login button)
+//        Espresso.onView(withId(R.id.popup_add).perform(click());
+
+//        // Wait until the monitor has been hit (stored in activity variable)
+//        Activity loginActivity = getInstrumentation().waitForMonitorWithTimeout(monitorHome, 5000);
+//        // If view is not null - then has launched successfully
+//        assertNotNull(loginActivity);
+//        // End activity
+//        loginActivity.finish();
+
+
+        // end of testLogin1 method
+    }
+
+
+//    @Test
+//    //  Systematic test #2 - launching Login Activity and signing in
+//    public void testVehicleDetails2()
+//    {
+////        // If view not null, then find element (login button)
+////        assertNotNull(nActivity.findViewById(R.id.loginEmail));
+//
+//
+//        Espresso.onView(withId(R.id.popup_title)).perform(typeText(reg2));
+//        // Close soft keyboard
+//        Espresso.closeSoftKeyboard();
+//        // Perform button click (on login button)
+////        Espresso.onView(withId(R.id.popup_add).perform(click());
+//
+////        // Wait until the monitor has been hit (stored in activity variable)
+////        Activity loginActivity = getInstrumentation().waitForMonitorWithTimeout(monitorHome, 5000);
+////        // If view is not null - then has launched successfully
+////        assertNotNull(loginActivity);
+////        // End activity
+////        loginActivity.finish();
+//
+//
+//    // end of testLogin2 method
+//    }
+
+
+//    @Test
+//    //  Systematic test #3 - launching Login Activity and signing in
+//    public void testVehicleDetails3()
+//    {
+////        // If view not null, then find element (login button)
+////        assertNotNull(nActivity.findViewById(R.id.loginEmail));
+//
+//
+//        Espresso.onView(withId(R.id.popup_title)).perform(typeText(reg3));
+//        // Close soft keyboard
+//        Espresso.closeSoftKeyboard();
+//        // Perform button click (on login button)
+////        Espresso.onView(withId(R.id.popup_add).perform(click());
+//
+////        // Wait until the monitor has been hit (stored in activity variable)
+////        Activity loginActivity = getInstrumentation().waitForMonitorWithTimeout(monitorHome, 5000);
+////        // If view is not null - then has launched successfully
+////        assertNotNull(loginActivity);
+////        // End activity
+////        loginActivity.finish();
+//
+//
+//    // end of testLogin1 method
+//    }
 
 
     // Tear down method
