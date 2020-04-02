7ec9cc9c808f4c6916c37a40585a8eba46e1a17c
sali011 <sali011@gold.ac.uk>
2020-04-02 14:52:00 +0100

    HomeActivity moved out of Activities

---
+++ app/src/main/java/com/example/vehicleservicemanagementapplication/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.example.vehicleservicemanagementapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vehicleservicemanagementapplication.R;
+
+public class HomeActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        // end of main method
+    }
+
+
+
+
+// end of class
+}
