daaa9587c78dce5a0e3fd1e1ddd5c4bfaa0b22c6
sali011 <sali011@gold.ac.uk>
2020-04-04 16:27:09 +0100

    Removing unnecessary classes and XML files

--- app/src/androidTest/java/com/example/vehicleservicemanagementapplication/HomeToSettingsIntegrationTest.java
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/HomeToSettingsIntegrationTest.java
@@ -7,10 +7,7 @@ import android.app.Instrumentation;
 
 import androidx.test.rule.ActivityTestRule;
 
-import com.example.vehicleservicemanagementapplication.About;
 import com.example.vehicleservicemanagementapplication.Activites.Home;
-import com.example.vehicleservicemanagementapplication.R;
-import com.example.vehicleservicemanagementapplication.Settings;
 
 import org.junit.After;
 import org.junit.Before;
