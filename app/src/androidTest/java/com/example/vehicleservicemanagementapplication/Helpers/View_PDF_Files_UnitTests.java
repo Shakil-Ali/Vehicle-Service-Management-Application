45ec15a8b02e755e82bc1537ec2fe1148da6d8aa
sali011 <sali011@gold.ac.uk>
2020-05-21 19:51:13 +0100

    View_PDF_Files_UnitTests - class created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Helpers/View_PDF_Files_UnitTests.java
@@ -0,0 +1,41 @@
+package com.example.vehicleservicemanagementapplication.Helpers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+// View PDF File Unit Tests Class
+public class View_PDF_Files_UnitTests
+{
+
+    // Setup method
+    @Before
+    public void setUp() throws Exception
+    {
+
+    // end of setup method
+    }
+
+    // Tear down method
+    @After
+    public void tearDown() throws Exception
+    {
+
+    // end of tear down method
+    }
+
+
+    // On create method
+    @Test
+    public void onCreate()
+    {
+
+    // end of on create method
+    }
+
+
+
+// end of class
+}
