d71a57ae37a92c0a1d2f3307ead4bd53ff5f6bde
sali011 <sali011@gold.ac.uk>
2020-05-21 20:47:29 +0100

    VehcileDetailToFileUploadIntegrationTest - class created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/VehicleDetailToFileUploadIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.example.vehicleservicemanagementapplication.Activites;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VehicleDetailToFileUploadIntegrationTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+    @Test
+    public void btn_action()
+    {
+
+    }
+
+
+
+}
