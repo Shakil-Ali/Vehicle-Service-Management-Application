c54b95cc51cc82284691c0c4d81ceeb6e6d08f2d
sali011 <sali011@gold.ac.uk>
2020-05-21 19:59:01 +0100

    uploadingPDFUnitTests - class created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Helpers/uploadingPDFUnitTests.java
@@ -0,0 +1,52 @@
+package com.example.vehicleservicemanagementapplication.Helpers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+// Uploading PDF Unit Tests Class
+public class uploadingPDFUnitTests
+{
+
+    // Setup method
+    @Before
+    public void setUp() throws Exception
+    {
+
+    // end of setup method
+    }
+
+
+    // Teardown method
+    @After
+    public void tearDown() throws Exception
+    {
+
+    // end of tear down method
+    }
+
+
+    // Get name method
+    @Test
+    public void getName()
+    {
+
+    // end of get name method
+    }
+
+
+    // Get url method
+    @Test
+    public void getUrl()
+    {
+
+    // end of url method
+    }
+
+
+
+// end of class
+}
