22f22bb211cc8a19b07f838d76babd05eb029311
sali011 <sali011@gold.ac.uk>
2020-05-21 19:39:14 +0100

    PDFUploadActivityUnitTests - class created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Helpers/PDFUploadActivityUnitTests.java
@@ -0,0 +1,61 @@
+package com.example.vehicleservicemanagementapplication.Helpers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+// PDFUploadActivityUnitTests class
+public class PDFUploadActivityUnitTests
+{
+
+    // Setup method
+    @Before
+    public void setUp() throws Exception
+    {
+
+    // end of setup method
+    }
+
+
+    // On-create method
+    @Test
+    public void onCreate()
+    {
+
+    // end of on-create method
+    }
+
+
+    // OnActivityResult method
+    @Test
+    public void onActivityResult()
+    {
+
+
+    // end of onactivityresult method
+    }
+
+
+    // Btn_action method
+    @Test
+    public void btn_action()
+    {
+
+
+    // end of btn_action method
+    }
+
+
+    // Teardown Method
+    @After
+    public void tearDown() throws Exception
+    {
+
+
+    // end of tear down method
+    }
+
+
+}
