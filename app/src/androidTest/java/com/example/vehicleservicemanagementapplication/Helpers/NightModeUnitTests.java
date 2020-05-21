e6fa4439308a683eb89b4d6f05b3b6531da7e592
sali011 <sali011@gold.ac.uk>
2020-05-21 18:18:10 +0100

    NightModeUnitTests - Class created

---
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Helpers/NightModeUnitTests.java
@@ -0,0 +1,30 @@
+package com.example.vehicleservicemanagementapplication.Helpers;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class NightModeUnitTests
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+
+    // end of setup method
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    // end of tear down method
+    }
+
+
+
+
+// end of night mode unit tests class
+}
