1741e6a53fca8a7a5addefc374948dea6290b34f
sali011 <sali011@gold.ac.uk>
2020-05-22 02:24:15 +0100

    VehicleDetailToFileUploadIntegrationTest - Code Cleaned

--- app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/VehicleDetailToFileUploadIntegrationTest.java
+++ app/src/androidTest/java/com/example/vehicleservicemanagementapplication/Activites/VehicleDetailToFileUploadIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.example.vehicleservicemanagementapplication.Activites;
 
+// https://www.youtube.com/watch?v=vXRoVIGttO4
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,5 +30,8 @@ public class VehicleDetailToFileUploadIntegrationTest
     }
 
 
-
+// end of class
 }
+
+
+
